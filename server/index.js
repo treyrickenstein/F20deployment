ab6d35c80138caa9be2ac006e42e26a02f48bb87
Trey Reichenstein <treyreichenstein@outlook.com>
2022-04-06 10:45:57 -0500

    server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const PORT = process.env.PORT || 4005
+
+app.listen(PORT, () => { console.log(`Listening on ${PORT}`)})
